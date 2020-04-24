3307646a0c112d71690e8da58fa699f075734ce9
subhajitsaha <subhajitsaha@192.168.1.105>
2020-04-24 21:14:04 +0530

    Initial commit

--- src/test/java/guru/springframework/SpringBootPostgreSQLApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootPostgreSQLApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.openshift.api;
+package guru.springframework;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringExampleApplicationTests {
+public class SpringBootPostgreSQLApplicationTests {
 
 	@Test
 	public void contextLoads() {
